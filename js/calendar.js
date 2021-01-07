b0a2ece6cfca42d350bea75b52e5ee208fe7d676
HichamHammouchi <hi.hammouchi@gmail.com>
2021-01-07 17:12:47 +0100

    events update

---
+++ js/calendar.js
@@ -0,0 +1,56 @@
+/* Attach events after the button has been loaded */
+window.addeventasync = function(){
+
+    /* Capture click on button */
+    addeventatc.register('button-click', function(obj){
+
+        /* Log */
+        console.log('button-click -> ' + obj.id);
+
+    });
+
+    /* Capture mouseover on button */
+    addeventatc.register('button-mouseover', function(obj){
+
+        /* Log */
+        console.log('button-mouseover -> ' + obj.id);
+
+    });
+
+    /* Capture mouseout on button */
+    addeventatc.register('button-mouseout', function(obj){
+
+        /* Log */
+        console.log('button-mouseout -> ' + obj.id);
+
+    });
+
+    /* Capture when dropdown is being displayed */
+    addeventatc.register('button-dropdown-show', function(obj){
+
+        /* Log */
+        console.log('button-dropdown-show -> ' + obj.id);
+
+    });
+
+    /* Capture when dropdown is being hidden */
+    addeventatc.register('button-dropdown-hide', function(obj){
+
+        /* Log */
+        console.log('button-dropdown-hide -> ' + obj.id);
+
+    });
+
+    /* Capture when an option in the dropdown is clicked */
+    addeventatc.register('button-dropdown-click', function(obj){
+
+        /* Log */
+        console.log('button-dropdown-click -> ' + obj.id + ', service -> ' + obj.service);
+
+        /* Track event click with e.g. Google Analytics (using analytics.js)
+        https://developers.google.com/analytics/devguides/collection/analyticsjs/events */
+        ga('send', 'event', 'MyEvent', 'play', 'Fall Campaign');
+
+    });
+
+};
